b80aa478882a67f8809b1a3108139618f1faa683
William Bruno <williambrass09@gmail.com>
2022-01-10 14:51:17 -0300

    project create

---
+++ backend/src/test/java/com/william/dsmovie/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.william.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
