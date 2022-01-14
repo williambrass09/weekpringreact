bb87c0a19ce9218c1ba1f5a60db6ab5f07a96560
William Bruno <williambrass09@gmail.com>
2022-01-14 17:52:05 -0300

    Save score

--- backend/src/main/java/com/william/dsmovie/entities/Movie.java
+++ backend/src/main/java/com/william/dsmovie/entities/Movie.java
@@ -1,13 +1,17 @@
 package com.william.dsmovie.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name= "tb_movie")
+@Table(name = "tb_movie")
 public class Movie {
 
 	@Id
@@ -17,6 +21,9 @@ public class Movie {
 	private Double score;
 	private Integer count;
 	private String image;
+	
+	@OneToMany(mappedBy = "id.movie")
+	private Set<Score> scores = new HashSet<>();
 	
 	public Movie() {
 		
@@ -70,7 +77,8 @@ public class Movie {
 	public void setImage(String image) {
 		this.image = image;
 	}
-	
-	
-	
+
+	public Set<Score> getScores() {
+		return scores;
+	}
 }
